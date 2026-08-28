03081a41415234090b7b9b6577fbbf92c2969c53
Jblue254 <japhethkiprono2020@gmail.com>
2026-08-28 23:06:26 +0300

    feat:structure of the web page and how it works

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Order
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(
+        request,
+        "product_list.html",
+        {"products": products}
+    )
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    return render(
+        request,
+        "product_detail.html",
+        {"product": product}
+    )
+
+
+@login_required
+def order_list(request):
+    orders = Order.objects.filter(
+        user=request.user
+    )
+
+    return render(
+        request,
+        "order_list.html",
+        {"orders": orders}
+    )
