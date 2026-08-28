e3d4d2c9352fb5a0bd802cb6da1a5214f5ff7df7
Jblue254 <japhethkiprono2020@gmail.com>
2026-08-28 23:36:54 +0300

    feat:add a test file to test the file

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .models import Category, Product
 
-# Create your tests here.
+
+class ProductViewTest(TestCase):
+
+    def setUp(self):
+        category = Category.objects.create(
+            name="Electronics"
+        )
+
+        Product.objects.create(
+            name="Laptop",
+            price=1000,
+            category=category
+        )
+
+    def test_product_list_view(self):
+        response = self.client.get("/")
+
+        self.assertEqual(
+            response.status_code,
+            200
+        )
+
+        self.assertContains(
+            response,
+            "Laptop"
+        )
