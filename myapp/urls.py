3a26527905fd80e6762f8e191e31179957387fac
Jblue254 <japhethkiprono2020@gmail.com>
2026-08-28 23:09:04 +0300

    feat:page redirection for the pages in the urls

---
+++ myapp/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path(
+        "",
+        views.product_list,
+        name="product_list"
+    ),
+
+    path(
+        "product/<int:pk>/",
+        views.product_detail,
+        name="product_detail"
+    ),
+
+    path(
+        "orders/",
+        views.order_list,
+        name="order_list"
+    ),
+]
