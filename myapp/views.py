39ddb7b1b231b14f35cd18d8e0135dd3e3e4cb53
Jblue254 <japhethkiprono2020@gmail.com>
2026-08-29 11:43:02 +0300

    feat:added a registration form

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Order
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
 
 def product_list(request):
@@ -32,4 +34,22 @@ def order_list(request):
         request,
         "order_list.html",
         {"orders": orders}
+    )
+
+def register(request):
+
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+
+    else:
+        form = RegisterForm()
+
+    return render(
+        request,
+        "register.html",
+        {"form": form}
     )
