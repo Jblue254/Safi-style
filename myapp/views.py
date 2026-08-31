d92a3fcce001f41188b83cc26a9f3c2d49e6bc54
Jblue254 <japhethkiprono2020@gmail.com>
2026-08-31 11:10:46 +0300

    feat:added a login structure for the admin

--- myapp/views.py
+++ myapp/views.py
@@ -4,8 +4,41 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
+def login_user(request):
 
+    if request.method == "POST":
+
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+
+            login(request, user)
+
+            if user.is_staff:
+                return redirect("/admin/")
+
+            return redirect("product_list")
+
+        else:
+            messages.error(
+                request,
+                "Invalid username or password"
+            )
+
+    return render(
+        request,
+        "login.html"
+    )
 def logout_user(request):
     logout(request)
     return redirect('/')
