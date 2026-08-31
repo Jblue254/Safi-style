b34d31004320db6a9d171605f2b2cc893dc552d3
Jblue254 <japhethkiprono2020@gmail.com>
2026-08-31 09:31:45 +0300

    feat:latest changes

--- myapp/models.py
+++ myapp/models.py
@@ -42,11 +42,16 @@ class OrderItem(models.Model):
         Order,
         on_delete=models.CASCADE
     )
+
     product = models.ForeignKey(
         Product,
         on_delete=models.CASCADE
     )
+
     quantity = models.PositiveIntegerField(default=1)
+
+    def total_price(self):
+        return self.product.price * self.quantity
 
     def __str__(self):
         return self.product.name
